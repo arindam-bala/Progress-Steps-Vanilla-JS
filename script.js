4cefacee1ae3a7027e09261cec9208c6669994bb
arindam-bala <arindambala576@gmail.com>
2022-02-16 23:59:50 +0530

    Adding addEventListener(click) to next and prev btn ids

--- script.js
+++ script.js
@@ -11,4 +11,14 @@ next.addEventListener('click', () => {
     if(currentActive > circles.length) {
         currentActive = circles.length;
     }
+    
+})
+
+prev.addEventListener('click', () => {
+    currentActive--;
+
+    if(currentActive > 1) {
+        currentActive = 1;
+    }
+    
 })
